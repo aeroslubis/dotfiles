a454e45faf68fb557187ce5d25f5546d1ecb47d2
aeroslubis <aeros.lubis@gmail.com>
2019-06-07 16:29:02 +0700

    add new config files

---
+++ zsh/.zsh/config/profile.zsh
@@ -0,0 +1,5 @@
+# Profile
+export NAME="Aeros Jarot Lubis"
+export NIK="1106120204960001"
+export DATEOFBIRTH="Banda Aceh, 13-11-1996"
+export PLACE="Jl.Meunasah Tuha, Ds.Tanjung Deah, Kc.Darussalam, Kb.Aceh Besar"
