011475c6c82ab1bb4b8c13a357b6c42f0cfcb49a
aeroslubis <aeros.lubis@gmail.com>
2019-12-17 19:53:04 +0700

    Add aliases

--- zsh/.zsh/configs/aliases.zsh
+++ zsh/.zsh/configs/aliases.zsh
@@ -32,7 +32,11 @@ alias clipcopy="xclip -in -selection clipboard"
 alias clippaste="xclip -out -selection clipboard"
 alias screen="miniterm.py"
 alias logs="tail -n 50 -f /var/log/Xorg.0.log"
-alias get-winpos="xdotool getwindowfocus getwindowgeometry"
+alias help="tldr"
+alias 2pdf="libreoffice --headless --invisible --convert-to pdf"
+alias imdb-rename="imdb-rename --data-dir $HOME/.cache/imdb-rename"
+alias music="ncmpcpp -q"
+alias elinks="elinks -config-dir $XDG_CONFIG_HOME/elinks/"
 
 # if user is not root, pass all these commands via sudo #
 if [ $UID -ne 0 ]; then
@@ -40,8 +44,27 @@ if [ $UID -ne 0 ]; then
     alias fdisk='sudo fdisk'
 fi
 
-alias help="tldr"
-alias 2pdf="libreoffice --headless --invisible --convert-to pdf"
-alias imdb-rename="imdb-rename --data-dir $HOME/.cache/imdb-rename"
-alias music="ncmpcpp -q"
-alias elinks="elinks -config-dir $XDG_CONFIG_HOME/elinks/"
+# oh-my-zsh systemd plugins
+user_commands=(
+    list-units is-active status show help list-unit-files
+    is-enabled list-jobs show-environment cat list-timers)
+
+sudo_commands=(
+    start stop reload restart try-restart isolate kill
+    reset-failed enable disable reenable preset mask unmask
+    link load cancel set-environment unset-environment
+    edit)
+
+for c in $user_commands; do; alias sc-$c="systemctl $c"; done
+for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
+for c in $user_commands; do; alias scu-$c="systemctl --user $c"; done
+for c in $sudo_commands; do; alias scu-$c="systemctl --user $c"; done
+
+# oh-my-zsh git plugins with a litle change
+alias ga='git add --verbose'
+alias gaa='git add --all --verbose'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gc='git commit -v'
+alias gd='git diff'
+alias gs='git status'
